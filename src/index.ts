d7c1bc93e0495c4d6cf8aad9302c9033f76f8314
Blue-Pheasant <voquylong.2k1@gmail.com>
2024-07-07 12:50:35 +0700

    export lib

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import { AxiosApiClient } from './AxiosApiClient';
+import { AxiosInterceptor } from './AxiosInterceptor';
+import { AxiosRequestConfig } from 'axios';
+import { IApiClient, IInterceptor } from './type';
+import { BaseInterceptor } from './BaseInterceptor';
+
+export {
+  AxiosApiClient,
+  AxiosInterceptor,
+  AxiosRequestConfig,
+  IApiClient,
+  IInterceptor,
+  BaseInterceptor,
+};
